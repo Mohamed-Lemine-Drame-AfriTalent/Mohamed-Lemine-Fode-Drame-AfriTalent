e8b7e6f9bc8469857276b771f15cc95ff57c0859
Mohamed-Lemine-Drame-AfriTalent <mohamedrame5@icloud.com>
2026-05-20 23:39:16 +0000

    JS : dark mode avec localStorage, navbar dynamique au scroll

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const toggleBtn = document.getElementById("theme-toggle");
+if(localStorage.getItem("theme") === "dark"){
+    document.body.classList.add("dark-mode");
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        localStorage.setItem("theme", "dark");
+    }else{
+        localStorage.setItem("theme", "light");
+    }
+
+});
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
+
+window.addEventListener("scroll", () => {
+    console.log("Scroll détecté");
+});
+
+const topBtn = document.getElementById("topBtn");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }   
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
